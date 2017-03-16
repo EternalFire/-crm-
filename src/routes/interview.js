d3733cb847afdfd09e5812b9aa6d0510c658ff34
hzh <hzhfire@qq.com>
2017-03-16 16:32:08 +0800

    move png file add component qr

--- src/routes/interview.js
+++ src/routes/interview.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import { connect } from 'dva'
+import { center } from '../utils'
+import QR from '../components/interview/qr'
 
 function Interview({dispatch, interview}) {
+  const {name, type, frontData} = interview
+
+  const qrProps = {
+    center: name,
+    centerName: center.getCenterName(name)
+  };
+
+  const renderQR = () => {
+    if (type === center.interviewType.qr) {
+      return (<QR {...qrProps} />)
+    }
+    return null;
+  }
+
+  const renderInterviewTable = () => {
+    if (type === center.interviewType.align) {
+      return (
+        <div>
+          <h1>Interview</h1>
+          <div>{type}</div>
+          <pre>{JSON.stringify(frontData)}</pre>  
+        </div>
+      )
+    }
+    return null;
+  }
+
   return (
     <div>
-      <h1>Interview</h1>
-      <div>{interview.type}</div>
-      <pre>{JSON.stringify(interview.frontData)}</pre>
+      {renderQR()}
+      {renderInterviewTable()}
     </div>
   )
 }
